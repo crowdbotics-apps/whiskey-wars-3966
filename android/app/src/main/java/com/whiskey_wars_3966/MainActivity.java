0a9ac8d0dc678b875074d5b90b2d1cabf6ce85ee
Crowdbotics <team@crowdbotics.com>
2019-05-29 02:32:46 +0000

    initial commit

---
+++ android/app/src/main/java/com/whiskey_wars_3966/MainActivity.java
@@ -0,0 +1,15 @@
+package com.whiskey_wars_3966;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "whiskey_wars_3966";
+    }
+}
